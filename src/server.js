42de966dc9fe6c21a4d0dbb1c0bb4ffea5e7ea1e
DiegoRamires <goncalves.ramires@gmail.com>
2019-10-02 16:39:50 -0300

    create routes file and configure express server to read json

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'hello dev' })
-})
+app.use(express.json())
+app.use(routes)
 
 const port = process.env.PORT || 3333
 app.listen(port, () => {
