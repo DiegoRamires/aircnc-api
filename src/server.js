024c103acfc209355fc132e9227dc6af159bd045
DiegoRamires <goncalves.ramires@gmail.com>
2019-10-05 18:31:23 -0300

    configure socket.io to send booking infos to dashboard admin

--- src/server.js
+++ src/server.js
@@ -18,6 +18,21 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 })
 
+const connectedUsers = {}
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query
+
+  connectedUsers[user_id] = socket.id
+})
+
+app.use((req, res, next) => {
+  req.io = io
+  req.connectedUsers = connectedUsers
+
+  return next()
+})
+
 app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
